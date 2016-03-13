94da3021aa1cda27b1568b081aa36ce893dadae3
Raj Shah <rjshah7769@gmail.com>
2016-03-13 04:58:23 -0400

    Update test.c

--- test.c
+++ test.c
@@ -1,9 +1,9 @@
+
 /* Header Files */
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
-/////////////////////
 
 /* Function Prototypes */
 void fibo(int n);
@@ -11,47 +11,37 @@ int divBy_op(int b);
 int primeCheck(int b);
 
 
-/////////////////////
-
-
 /* main function to call Fibonacci Sequence Generator */
 // This is where the testing takes place
 void main()
 {
     int totalNumber = 0;
-    printf("Enter the number of Fibonacci numbers required:\n");
+    printf("Enter the number of fibonacci numbers required:\n");
     scanf ("%d", &totalNumber);
-    if totalNumber < 2
-    print ("please input a number greater than 2");
+    if (totalNumber < 2)
+    printf ("please input a number greater than 2");
     else
     printf("Fibonacci Sequence \n");
     fibo(totalNumber);
 }
 
-/////////////////////
 
 /* Function Declarations*/
 
 void fibo(int n){
-///////////////////////////////////////////////////////////////////////
-// Purpose:    Generates Fibonacci Sequence
-// Input:   n
-// Returns:   Nothing / void
-// Calls:  checkDivisible(), checkPrime();
-// Notes:  printing the value F(n) when not prime or divisible be 3 or 5.
-///////////////////////////////////////////////////////////////////////
+
     int a = 0;
     int b = 1;
     int i = 0;
     //printf("%i \n",1);  // 1 is the chosen Starting point of the Sequence
 
-    for  (i = 0; i < (n - 1); i++ {
+    for  (i = 0; i < (n - 1); i++) {
         int t = a;
         a = b;
         b += t;     // b = F(n): is the latest number of the Fib Sequence being generated
 
-        if( divBy_op(b) ) {}  // Checks if b is divisible by 3 or 5
-        else if( primeCheck(b) ) {}  // Checks if b is prime
+        if( primeCheck(b) ) {}  // Checks if b is divisible by 3 or 5
+        else if( divBy_op(b) ) {}  // Checks if b is prime
         else {
             printf("%i \n",b);
         }
@@ -61,19 +51,12 @@ void fibo(int n){
 }
 
 int divBy_op(int b){
-///////////////////////////////////////////////////////////////////////
-// Purpose:   Checks if the number is divisible by 3 or 5
-// Input:     b
-// Returns:   True(1) if input is divisible by 3 or 5. False(0) otherwise.
-// Notes:     prints ...
-// ... "Buzz" when b is divisible by 3.
-// ... "Fizz" when b is divisible by 5.
-///////////////////////////////////////////////////////////////////////
-    if (b%3 == 0){
+
+    if (b % 3 == 0){
         printf("Buzz \n");
         return 1;
     }
-    if(b%5 == 0){
+    if(b % 5 == 0){
         printf("Fizz \n");
         return 1;
     }
@@ -81,19 +64,15 @@ int divBy_op(int b){
 }
 
 int primeCheck(int b){
-///////////////////////////////////////////////////////////////////////
-// Purpose:   Checks if the number is prime
-// Input:     b
-// Returns:   True(1) if input is prime. False(0) otherwise.
-// Notes:     prints ...
-// ... "BuzzFizz" when b is prime.
-///////////////////////////////////////////////////////////////////////
+
     int c;
 
-   for ( c = 2 ; c <= b ; c++ ){
-        if ( b%c == 0 )
+   for ( c = 2 ; c <= b/2 ; c++ ){
+        if (b % c == 0 )
         return 0;
+       //printf("BuzzFizz\n");
    }
-   printf("BuzzFizz \n");
+   printf("BuzzFizz\n");
    return 1;
 }
+
